fe5b2df49164d0ac4576fe5672a263a54ba4bb95
Jeremy Jamer <j3remyz1on@gmail.com>
2024-12-30 07:35:25 +0800

    Initial commit

---
+++ src/main/java/net/crema/bbcc/BitchesBeCrayCray.java
@@ -0,0 +1,17 @@
+package net.crema.bbcc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BitchesBeCrayCray implements ModInitializer {
+	public static final String MOD_ID = "bitches-be-cray-cray";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/crema/bbcc/BitchesBeCrayCrayClient.java
@@ -0,0 +1,10 @@
+package net.crema.bbcc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BitchesBeCrayCrayClient implements  ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
